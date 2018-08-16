7aa571486a4b81eb11bd1a7a56cfe087fb92d72b
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-08-15 22:02:09 -0700

    tried to start on testing, but files not setup correctly

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='snakes_cafe',
+    version='0.0.1',
+    packages=['snakes_cafe'],
+    url='https://github.com/sjschmidt44/data_structures_and_algorithms',
+    license='MIT',
+    author='Chris L Chapman',
+    author_email='ChristopherLChapman42@gmail.com',
+    description='Snakes Cafe menu and order system using the Python standard library'
+)
