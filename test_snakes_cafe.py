7731ec2a5a3b8d319d2702b410436f23e575f934
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-08-29 21:58:56 -0700

    revist to implement unfinished features

--- test_snakes_cafe.py
+++ test_snakes_cafe.py
@@ -1,28 +1,82 @@
-from .snake_cafe import get_menu, parse_user_input, get_order
-# from .snakes_cafe import get_menu
-# from .snakes-cafe import get_order
-# from .snakes-cafe import parse_user_input
+from .snakes_cafe import parse_user_input, get_order, show_menu
 
 
 # greeting() only has a print output. No additional testing.
 # show_menu() only does a print of output. No additonal testing.
 # display_order() only has print output. No additional testing.
 # goodybye only() has print output. No additonal testing.
+# get_menu() is for future features.
 
-def test_get_menu_exists():
-    """get_menu allows us to import a cvs file that holds the data of our menu.
+
+def test_alive():
+    """ Is our test file able to run
     """
-    assert get_menu
+    pass
+
+
+# get_menu is for future features
+# def test_get_menu_exists():
+#     """get_menu allows us to import a cvs file that holds the data of our menu.
+#     """
+#     assert get_menu
+
+def test_show_menu_exists():
+    """ Can we see the show_menu function
+        menu order remove
+    """
+    assert show_menu
+
+
+def test_show_menu_default():
+    """ Testing what is he first, and common way to call this command
+    """
+    assert show_menu('menu')
 
 
 def test_parse_user_input_exists():
-    """parse_user_input function does a lot of the work in this code
+    """ Do we see Parse_user_input function
     """
     assert parse_user_input
 
 
+def test_parse_user_input_quit():
+    """ On quit it should just return the string back to us
+    """
+    input = 'quit'
+    output = parse_user_input(input)
+    assert input == output
+
+
+def test_parse_user_input_menu():
+    """ On quit it should just return the string back to us
+    """
+    input = 'menu'
+    output = parse_user_input(input)
+    expected = 'show_menu | menu'
+    assert output == expected
+
+
+def test_parse_user_input_order():
+    """ On quit it should just return the string back to us
+    """
+    input = 'order'
+    output = parse_user_input(input)
+    expected = 'display_order'
+    assert output == expected
+
+
+def test_parse_user_input_remove():
+    """ On quit it should just return the string back to us
+    """
+    input = 'remove'
+    output = parse_user_input(input)
+    result = output.split(' ')
+    expected = 'remove_item'
+    assert result[0] == expected
+
+
 def test_get_order_exists():
-    """get_order function is needed for adding items to user's order
+    """ get_order function is needed for adding items to user's order
     """
     assert get_order
 
